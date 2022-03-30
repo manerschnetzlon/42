70e459f9b4ab9356653490df350f35680e0d35b1
Manon Schnetzler <bonsoir@mailo.com>
2022-03-30 16:09:56 -0400

    ft_strmapi

---
+++ 01_libft/ft_strmapi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  size_t  i;
+  char  *new_str;
+
+  if (!s)
+    return (NULL);
+  new_str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+  if (!new_str)
+    return (NULL);
+  i = 0;
+  while (s[i])
+  {
+    new_str[i] = (*f)(i, s[i]);
+    i++;
+  }
+  new_str[i] = '\0';
+  return (new_str);
+}
--- 01_libft/main.c
+++ 01_libft/main.c
@@ -5,11 +5,36 @@
 #include <strings.h>
 #include <stdlib.h>
 
+char to_upcase(unsigned int i, char s)
+{
+  return (s - (32 - i + i));
+}
+
 int main()
 {
   // char s1[] = "eeeeeeHello Manereeeeeaeeee";
   // char s2 = 'e';
+  char str[] = "hello";
 
   // ft_split(s1, s2);
-  ft_itoa(-1234560);
+  // ft_itoa(-1234560);
+
+  char (*f)(unsigned int, char) = &to_upcase;
+
+  printf("%s\n", ft_strmapi(str, (*f)));
 }
+
+// char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+
+// static int triple(int a)
+// {
+//   return a * 3;
+// }
+
+// int main(void)
+// {
+//   int (*pt)(int) = &triple;
+
+//   printf("%d.\n", (*pt)(3));
+//   return 0;
+// }
