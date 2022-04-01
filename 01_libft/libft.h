a51ebbf512738a196669de24cfaf0b28d6f84c78
Manon Schnetzler <bonsoir@mailo.com>
2022-04-01 15:42:41 -0400

    ft_lstnew WIP

--- 01_libft/libft.h
+++ 01_libft/libft.h
@@ -2,6 +2,11 @@
 #define LIBFT_H_
 
 typedef unsigned long size_t;
+typedef struct s_list
+{
+  void *content;
+  struct s_list *next;
+} t_list;
 
 int ft_atoi(const char *str);
 void ft_bzero(void *s, size_t n);
@@ -12,6 +17,7 @@ int ft_isalnum(char c);
 int ft_isascii(char c);
 int ft_isprint(char c);
 char *ft_itoa(int n);
+t_list *ft_lstnew(void *content);
 void *ft_memchr(const void *s, int c, size_t n);
 void *ft_memcpy(void *dst, const void *src, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
