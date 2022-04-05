d98a333c28a75e2e6990aaf53bad43e41cbb28d0
Manon Schnetzler <bonsoir@mailo.com>
2022-04-05 17:11:45 +0200

    ft_memmove FIX again

--- 01_libft/ft_memmove.c
+++ 01_libft/ft_memmove.c
@@ -12,15 +12,6 @@ void *ft_memmove(void *dst, const void *src, size_t len)
       len--;
     }
   }
-  else if ((dst < src) && (dst + len > src))
-  {
-    i = 0;
-    while (i < len)
-    {
-      ((char *)dst)[i] = ((char *)src)[i];
-      i++;
-    }
-  }
   else
     ft_memcpy(dst, src, len);
   return (dst);
