42f11e78814c6e2722b12da6bd20398468975589
Manon Schnetzler <bonsoir@mailo.com>
2022-05-30 18:11:12 +0200

    cleaning B2bR script

--- 42-cursus/03_Born2beRoot/monitoring.sh
+++ 42-cursus/03_Born2beRoot/monitoring.sh
@@ -5,10 +5,6 @@ TOTAL_MEM=$(free -m | grep "Mem" | awk '{print $2}')
 USED_MEM=$(free -m | grep "Mem" | awk '{print $3}')
 PERC_MEM=$(free -m | grep "Mem" | awk '{printf "%.2f", ($3/$2*100)}')
 echo "#Memory Usage: $USED_MEM/$TOTAL_MEM MB ($PERC_MEM%)"
-#DISK_TOTAL=$(df -m | grep '^/dev' | awk '{total += $2} END {print total}')
-#DISK_USED=$(df -m | grep '^/dev' | awk '{used += $3} END {print used}')
-#echo "total = $DISK_TOTAL"
-#echo "used = $DISK_USED"
 echo "#Disk Usage: $(df -m | grep '^/dev' | awk '{total += $2} {used += $3} END {printf "%i/%iMB (%.2f%%)", used, total, used/total*100}')"
 echo "#CPU load: $(mpstat | grep 'all' | awk '{print (100-$13)}')%"
 echo "#Last boot: $(who -b | awk '{print $3 " " $4}' )"
